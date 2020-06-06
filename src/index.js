4980fbb85bef8bc26c48e861d6e268c6550e3284
Jesper Balle <jesper.balle@valtech.com>
2020-06-06 21:47:30 +0200

    Initial render particles

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-import { setupDetection, detect } from "./detection-cco-ssd";
-// import { setupDetection, detect } from "./detection-automl";
+//import { setupDetection, detect } from "./detection-cco-ssd";
+//import { setupDetection, detect } from './detection-pixfinder';
+import { setupDetection, detect } from "./detection-marvinj";
 import { setupVideo } from "./video";
 import { setupRender, renderResults } from "./render";
+import { canvas, video } from "./elements";
+import { renderParticles } from "./particles.js";
+
+const data = {
+  canvas: canvas,
+  canvasCtx: canvas.getContext("2d"),
+};
 
 async function setup() {
-  const model = await setupDetection();
-  const video = await setupVideo();
-  const ctx = await setupRender();
-  detectFrame(model, video, ctx);
+  data.model = await setupDetection();
+  data.video = await setupVideo();
+  data.ctx = await setupRender();
+
+  canvas.addEventListener("dblclick", () => {
+    if (data.raf) {
+      stop();
+    } else {
+      start();
+    }
+  });
 }
 
-async function detectFrame(model, video, ctx) {
-  const results = await detect(model, video);
-  await renderResults(results, ctx);
+async function loop() {
+  data.canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  const results = await detect(data.model, data.canvas);
+  await renderResults(results, data.ctx);
 
-  requestAnimationFrame(async () => detectFrame(model, video, ctx));
+  data.raf = requestAnimationFrame(loop);
 }
 
-setup().then(() => console.log("ok"));
+async function start() {
+  console.log("Starting...");
+  data.raf = requestAnimationFrame(loop);
+}
+
+async function stop() {
+  console.log("Stopping...");
+  cancelAnimationFrame(data.raf);
+  data.raf = null;
+}
+
+// setup().then(start).then(() => console.log("ok"));
+renderParticles();
