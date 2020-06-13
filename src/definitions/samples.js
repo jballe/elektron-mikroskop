7018d9a2e7c273370ac0c70aa543432c490e33bb
Jesper Balle <jesper.balle@valtech.com>
2020-06-13 20:49:24 +0200

    Add samples

--- src/definitions/samples.js
+++ src/definitions/samples.js
@@ -1,17 +1,76 @@
 const samples = {
+  test: {
+    title: "Gerningsstedet",
+    result: {
+      c: 2,
+      d: 1,
+      e: 2,
+    },
+  },
   ronnie: {
-    title: "Ronnnie",
+    title: "Ronnnie Roligan",
     result: {
       a: 2,
       b: 3,
+      f: 2
     },
   },
   pudderkvast: {
     title: "Fru Pudderkvast",
     result: {
-      a: 1,
+      b: 1,
       c: 3,
+      d: 2,
+      e: 1
     },
+  },
+  mai: {
+    title: "Mai (Emmas mor)",
+    result: {
+      a: 1,
+      c: 2,
+      d: 2,
+      e: 2,
+      f: 1
+    }
+  },
+  trygvi: {
+    title: "Trygvi (Runís far)",
+    result: {
+      b: 1,
+      c: 2,
+      d: 1,
+      e: 2,
+      f: 1
+    }
+  },
+  adela: {
+    title: "Adela (Vincents mor)",
+    result: {
+      a: 2,
+      c: 1,
+      d: 1,
+      e: 2,
+      f: 1
+    }
+  },
+  vagn: {
+    title: "Camping Vagn",
+    result: {
+      b: 2,
+      c: 2,
+      d: 1,
+      e: 2
+    }
+  },
+  riffel: {
+    title: "Hanne Riffel",
+    result: {
+      c: 1,
+      d: 2,
+      e: 1,
+      f: 3
+    }
   },
 };
 export default samples;
